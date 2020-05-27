3c8e424d5d9eec6dbe2e85a7e55007fbe22307c4
Jere <jere.chiosso@gmail.com>
2020-05-27 11:58:56 -0300

    Agregada imagen a pantalla de inicio

--- src/frontend/components/wellcome/wellcome.js
+++ src/frontend/components/wellcome/wellcome.js
@@ -11,7 +11,10 @@ const WellcomeScreen = function ({ logedIn, onLogin }) {
     return (
         <div id="wellcome-container">
             <h1 id="wellcome-header"> Mercado Tracker </h1>
-            <p> Elegí un producto de mercadolibre y te avisamos cuando cambie el precio </p>
+
+            <img id='wellcome-image' src='start-image.png' alt='Imagen de presentacion' />
+            
+            <p> Elegí un producto de mercadolibre y te avisamos cuando cambie el precio <strong>¡Así de simple!</strong> </p>
 
             <div id="wellcome-actions">
                 <Link to={`/signin`}><button> Registrarse </button></Link>
